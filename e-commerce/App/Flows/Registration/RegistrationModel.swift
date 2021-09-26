129eabb05bc6526ace29d4e2e696c6d50c871040
MolkovD <MolkovD@ya.ru>
2021-09-26 14:04:51 +0300

    add module registration

---
+++ e-commerce/App/Flows/Registration/RegistrationModel.swift
@@ -0,0 +1,14 @@
+//
+//  RegistrationModel.swift
+//  e-commerce
+//
+//  Created by Denis Molkov on 26.09.2021.
+//
+
+import Foundation
+
+protocol RegistrationView {}
+
+protocol RegistrationViewPresenter {
+    init(view: RegistrationView)
+}
